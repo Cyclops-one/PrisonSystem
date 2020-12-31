92871b67fecf1248cd9fec2c0c8311695a14ffa1
Shakawat Hussain <cyberlord@programmer.net>
2020-12-31 15:24:48 +0600

    31thDec

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prisoner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Random"].ConnectionString);
+            connection.Open();
+            string gen = null;
+            if (txtmale.Checked)
+            {
+                gen = txtmale.Text;
+            }
+            else
+            {
+                gen = txtfemale.Text;
+            }
+            string sql = "INSERT INTO Criminals(Name,PriosnerId,Gender,DateofBirth,CrimeDescription,Punishment,CellNo,BloodGroup,Address)" +
+                " VALUES('"+txtname.Text+"','"+txtprisonerid.Text+"','"+gen+"','"+dob.Text+"','"+txtcrimedescription.Text+"','"+txtpunishment.Text+"','"+txtcellno.Text+"','"+txtbloodgroup.Text+"','"+txtaddress.Text+"')";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Prisoner Added successfully!");
+            Outform f2 = new Outform();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ Outform.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prisoner
+{
+    public partial class Outform : Form
+    {
+        public Outform()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Random"].ConnectionString);
+                string query;
+                SqlCommand SqlCommand;
+                SqlDataReader reader;
+                SqlDataAdapter ad = new SqlDataAdapter();
+                con.Open();
+                query = "Select * from Criminals";
+
+                SqlCommand = new SqlCommand(query, con);
+
+                ad.SelectCommand = new SqlCommand(query, con);
+                reader = SqlCommand.ExecuteReader();
+
+                dataGridView1.DataSource = reader;
+                dataGridView1.DataBindings();
+  }
+
+            catch
+
+            {
+
+                MessageBox.Show("No Record Found");
+
+            }
+
+        }
+    }
+}
